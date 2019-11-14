555c5e198b0d004d03fc6b824d6ad517d4e0738a
tanhui <tanhui@corp-ci.com>
2019-11-14 17:47:44 +0800

    fix sign different

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,7 @@ if [ ! -d "$HOME/.ci123" ]; then
     ./cid-linux add-genesis-account 0x204bCC42559Faf6DFE1485208F7951aaD800B313 100000
     ./cid-linux add-genesis-account 0x505A74675dc9C71eF3CB5DF309256952917E801e 100000
     ./cid-linux add-genesis-account 0xD1a14962627fAc768Fe885Eeb9FF072706B54c19 100000
+    
 fi
 
 nohup ./cid-linux start > cid-output 2>&1 &
