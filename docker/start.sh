c9d60558c49b553cdf7189510f5211174b397a9b
tanhui <tanhui@corp-ci.com>
2019-12-03 14:17:58 +0800

    no message

--- docker/start.sh
+++ docker/start.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-HOME_DIR="$HOME/.ci123"
+HOME_DIR="$HOME"
 if [ $CI123_HOME ];then
     HOME_DIR=$CI123_HOME
 fi
+
+CLI_HOME="${HOME_DIR}/cli"
+CID_HOME="${HOME_DIR}/cid"
+
 # genesis file
-echo $HOME_DIR/genesis.json
-if [ ! -f $HOME_DIR/genesis.json ]; then
-    ./cid-linux init --home=$HOME_DIR
+if [ ! -f $CID_HOME/genesis.json ]; then
+    ./cid-linux init --home=$CID_HOME
 fi
-./cid-linux add-genesis-account 0x204bCC42559Faf6DFE1485208F7951aaD800B313 100000 --home=$HOME_DIR
-./cid-linux add-genesis-account 0x505A74675dc9C71eF3CB5DF309256952917E801e 100000 --home=$HOME_DIR
-./cid-linux add-genesis-account 0xD1a14962627fAc768Fe885Eeb9FF072706B54c19 100000 --home=$HOME_DIR
+./cid-linux add-genesis-account 0x204bCC42559Faf6DFE1485208F7951aaD800B313 100000 --home=$CID_HOME
+./cid-linux add-genesis-account 0x505A74675dc9C71eF3CB5DF309256952917E801e 100000 --home=$CID_HOME
+./cid-linux add-genesis-account 0xD1a14962627fAc768Fe885Eeb9FF072706B54c19 100000 --home=$CID_HOME
 
 # start
-nohup ./cid-linux start --home=$HOME_DIR > cid-output 2>&1 &
+nohup ./cid-linux start --home=$CID_HOME > cid-output 2>&1 &
 
-./cli-linux rest-server --home=$HOME_DIR > rest-output 2>&1
+./cli-linux rest-server --home=$CLI_HOME > rest-output 2>&1
