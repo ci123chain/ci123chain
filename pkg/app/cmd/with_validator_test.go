438866c83d58ce1a735831a14dbcd68147a4d7b0
tanhui <tanhui@corp-ci.com>
2020-02-17 14:21:36 +0800

    no message

--- pkg/app/cmd/with_validator_test.go
+++ pkg/app/cmd/with_validator_test.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/tanhuiya/ci123chain/pkg/validator"
 	"github.com/tendermint/tendermint/crypto/secp256k1"
+	"io"
 	"testing"
 
 	"github.com/tendermint/go-amino"
@@ -18,10 +20,10 @@ func init() {
 }
 
 // {"type":"tendermint/PrivKeySecp256k1","value":"oQLmM5pM5wL78a6LJntQY8tPGQPpp050udIA5YZMkCc="}
-func TestValidator(t *testing.T) {
+// 通过 json 文件里编码后的私钥解码
+func TestGenValidator(t *testing.T) {
 	// 私钥编码后的数据
 	privBz := "oQLmM5pM5wL78a6LJntQY8tPGQPpp050udIA5YZMkCc="
-
 	privStr := fmt.Sprintf(`{"type":"%s","value":"%s"}`, secp256k1.PrivKeyAminoName, privBz)
 	var privateKey secp256k1.PrivKeySecp256k1
 	// 生成的对象privateKey 直接使用
@@ -35,3 +37,18 @@ func TestValidator(t *testing.T) {
 
 	fmt.Printf("%s", jsonBytes)
 }
+
+// 通过secret 构造私钥
+func TestGenValidator2(t *testing.T) {
+	// 私钥编码后的数据
+	privSecret := "nodenodenodenodenodenodenodenode"
+	sbz := []byte(privSecret)
+	var b32 [32]byte
+	io.ReadFull(bytes.NewReader(sbz), b32[:])
+	privateKey := secp256k1.PrivKeySecp256k1(b32)
+
+	pv := validator.GenFilePV("", "", privateKey)
+	jsonBytes, _ := cdc.MarshalJSONIndent(pv.Key, "", "  ")
+	fmt.Printf("%s", jsonBytes)
+}
+
