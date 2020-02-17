c9ff67b276aa8e71b38ba7fc6346cf2dcf865da2
tanhui <tanhui@corp-ci.com>
2020-02-17 10:26:36 +0800

    fix script

--- testdocker/start-output.sh
+++ testdocker/start-output.sh
@@ -1,6 +1,5 @@
-cp -r node0-template node0
-cp -r node1-template node1
-cp -r node2-template node2
+../build/cid boot-gen --chain-pre=node --node-num=3 --output-dir=.
+
 mkdir gateway
 docker-compose -f part1.yaml up -d
 sleep 15
