88a00897a54ee18b2a60f0ea0736ced3ecbb405f
hanshaoxin <hanshaoxin@corp-ci.com>
2020-12-16 10:48:59 +0800

    fix error

--- pkg/distribution/client/rest/rest.go
+++ pkg/distribution/client/rest/rest.go
@@ -29,7 +29,7 @@ func checkDelegatorAddressVar(w http.ResponseWriter, r *http.Request) (string, b
 }
 
 func checkValidatorAddressVar(w http.ResponseWriter, r *http.Request) (string, bool) {
-	address := r.FormValue("account_address")
+	address := r.FormValue("validator_address")
 	checkErr := util.CheckStringLength(42, 100, address)
 	if checkErr != nil {
 		rest.WriteErrorRes(w,types.ErrBadAddress(types.DefaultCodespace, checkErr))
