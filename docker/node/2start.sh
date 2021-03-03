e8786867538987fa8980ebabe02afbc6d6110449
tanhui <tanhui@corp-ci.com>
2021-03-03 16:25:21 +0800

    修改初始化账户余额

--- docker/node/2start.sh
+++ docker/node/2start.sh
@@ -35,14 +35,12 @@ if [ ! -f $CI_HOME/config/genesis.json ]; then
       #./cid-linux init
       /opt/cid-linux init --home=$CI_HOME --chain_id=$CI_CHAIN_ID --validator_key=$CI_VALIDATOR_KEY
 
-      /opt/cid-linux add-genesis-account 0x204bCC42559Faf6DFE1485208F7951aaD800B313 10000000000 --home=$CI_HOME
+      /opt/cid-linux add-genesis-account 0x204bCC42559Faf6DFE1485208F7951aaD800B313 1000000000000000000000 --home=$CI_HOME
       # a78a8a281d160847f1ed7881e5497e1a98ccd4fe6ba9ce918630f93a44e09793
-      /opt/cid-linux add-genesis-account 0x3F43E75Aaba2c2fD6E227C10C6E7DC125A93DE3c 10000000000 --home=$CI_HOME
+      /opt/cid-linux add-genesis-account 0x3F43E75Aaba2c2fD6E227C10C6E7DC125A93DE3c 1000000000000000000000 --home=$CI_HOME
       # 2b452434ac4f7cf9c5d61d62f23834f34e851fb6efdb8d4a8c6e214a8bc93d70
 
-      /opt/cid-linux add-genesis-account 0xB6727FCbC60A03A6689AEE6E5fBC83a7FDc9beBf 10000000000 --home=$CI_HOME
-
-      /opt/cid-linux add-genesis-validator 0x3F43E75Aaba2c2fD6E227C10C6E7DC125A93DE3c 8000000 $CI_PUBKEY 1 40 5 --home=$CI_HOME
+      /opt/cid-linux add-genesis-validator 0x3F43E75Aaba2c2fD6E227C10C6E7DC125A93DE3c 800000000 $CI_PUBKEY 1 40 5 --home=$CI_HOME
 
       if [ $GENESIS_SHARED ]; then
           /opt/cid-linux add-genesis-shard "$GENESIS_SHARED"
