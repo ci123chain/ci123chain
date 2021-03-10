ce9fe2098dc0a0678a19fb84b6e689174289df60
hanshaoxin <hanshaoxin@corp-ci.com>
2021-03-10 19:19:09 +0800

    remove servername

--- pkg/gateway/redissource/source.go
+++ pkg/gateway/redissource/source.go
@@ -119,7 +119,7 @@ func getOption(statedb string) (*redis.Options, error) {
 			Addr: auths[0],
 			DB:   0,
 		}
-		opt.TLSConfig = &tls.Config{ServerName: auths[0], InsecureSkipVerify: true}
+		opt.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 		return opt, nil
 	} else { // admin:password@192.168.2.89:5984
 		info := auths[0] // admin:password
@@ -129,7 +129,7 @@ func getOption(statedb string) (*redis.Options, error) {
 				Addr: auths[1],
 				DB:   0,
 			}
-			opt.TLSConfig = &tls.Config{ServerName: auths[1], InsecureSkipVerify: true}
+			opt.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 			return opt, nil
 		} else {
 			opt := &redis.Options{
@@ -138,7 +138,7 @@ func getOption(statedb string) (*redis.Options, error) {
 				Password:           userandpass[1],
 				DB:                 0,
 			}
-			opt.TLSConfig = &tls.Config{ServerName: auths[1], InsecureSkipVerify: true}
+			opt.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 			return opt, nil
 		}
 	}
