ad1a1dd2781e4b5ee308631f373f7d3066e0e2dd
hanshaoxin <hanshaoxin@corp-ci.com>
2021-05-25 14:36:32 +0800

    add params validate

--- pkg/params/types/subspace.go
+++ pkg/params/types/subspace.go
@@ -231,9 +231,9 @@ func (s Subspace) SetParamSet(ctx sdk.Context, ps ParamSet) {
 		// so this method will not be called frequently
 		v := reflect.Indirect(reflect.ValueOf(pair.Value)).Interface()
 
-		//if err := pair.ValidatorFn(v); err != nil {
-		//	panic(fmt.Sprintf("value from ParamSetPair is invalid: %s", err))
-		//}
+		if err := pair.ValidatorFn(v); err != nil {
+			panic(fmt.Sprintf("value from ParamSetPair is invalid: %s", err))
+		}
 
 		s.Set(ctx, pair.Key, v)
 	}
