2e0435e61fe53307b3be07229d2a1961806f3501
tanhui <tanhui.master@gmail.com>
2021-09-27 14:50:31 +0800

    aaaa

--- cmd/test/tcp_test.go
+++ cmd/test/tcp_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net"
 	"testing"
@@ -13,6 +14,32 @@ func TestTcp(t *testing.T)  {
 		fmt.Println(err)
 	}
 	//conn.Write([]byte("aaaa"))
+	select {
+	}
+}
+
+func TestTcpTls(t *testing.T)  {
+	var cert tls.Certificate
+	config := tls.Config{
+		Certificates:       []tls.Certificate{cert},
+		ServerName:         "weelinknode1c.gw002.oneitfarm.com:7443",
+		InsecureSkipVerify: true,
+	}
+
+	//remote, err := tls.Dial("tcp", remoteServer, &config)
+
+	remoteServer := "weelinknode1c.gw002.oneitfarm.com:7443"
+
+	remote, err := tls.DialWithDialer(&net.Dialer{
+		Timeout: time.Second * time.Duration(10),
+	}, "tcp", remoteServer, &config)
+
+	if err != nil {
+		fmt.Printf("remote tls dial fail.  %s", err.Error())
+		return
+	}
+
+	defer remote.Close()
 	select {
 	}
 }
