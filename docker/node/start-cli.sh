167530ab8df1221594e61028886d1eb88154eb36
tanhui <tanhui.master@gmail.com>
2021-10-27 17:41:27 +0800

    fix default ethchainid

--- docker/node/start-cli.sh
+++ docker/node/start-cli.sh
@@ -4,6 +4,11 @@ then
    CI_HOME="/opt/ci123chain"
 fi
 
+if [ -z $CI_ETH_CHAIN_ID ];
+then
+   CI_ETH_CHAIN_ID=7880
+fi
+
 if [ -z $CI_TOKENNAME ];
 then
    CI_TOKENNAME="WLK"
@@ -13,5 +18,8 @@ CI_LOGDIR=$CI_HOME/logs
 if [ ! -d $CI_LOGDIR ]; then
     mkdir -p $CI_LOGDIR
 fi
+
+echo "export CI_ETH_CHAIN_ID=$CI_ETH_CHAIN_ID" >> /etc/profile
+source /etc/profile
 
 /opt/cli-linux rest-server --laddr=tcp://0.0.0.0:80 >> $CI_LOGDIR/rest-output.log 2>&1
