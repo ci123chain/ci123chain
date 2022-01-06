0d9bc2a558e6566470df7176f5d6a29e9a9bd44c
huangyu9784 <huangyu@corp-ci.com>
2022-01-06 17:32:26 +0800

    add fixExportFile for Gravity module

--- docker/node/testFixExportFile_test.go
+++ docker/node/testFixExportFile_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	acc_types "github.com/ci123chain/ci123chain/pkg/account/types"
 	app_types "github.com/ci123chain/ci123chain/pkg/app/types"
+	gravity_types "github.com/ci123chain/ci123chain/pkg/gravity/types"
 	"github.com/tendermint/tendermint/types"
 	"io/ioutil"
 	"testing"
@@ -20,6 +21,8 @@ func TestFixFile(t *testing.T) {
 	type GenesisState map[string]json.RawMessage
 	var genesisState GenesisState
 	cdc.MustUnmarshalJSON(exportFile.AppState, &genesisState)
+
+	//fix accounts
 	var accGenesisState, newAccGenesisState acc_types.GenesisState
 	cdc.MustUnmarshalJSON(genesisState["accounts"], &accGenesisState)
 	for i := range accGenesisState {
@@ -29,6 +32,16 @@ func TestFixFile(t *testing.T) {
 	}
 	newAccGenesisStateBz := cdc.MustMarshalJSON(newAccGenesisState)
 	genesisState["accounts"] = newAccGenesisStateBz
+
+	//fix gravity
+	var graGenesisState, newGraGenesisState gravity_types.GenesisState
+	cdc.MustUnmarshalJSON(genesisState["gravity"], &graGenesisState)
+	newGraGenesisState = *gravity_types.DefaultGenesisState()
+	newGraGenesisState.DelegateKeys = graGenesisState.DelegateKeys
+	newGraGenesisStateBz := cdc.MustMarshalJSON(newGraGenesisState)
+	genesisState["gravity"] = newGraGenesisStateBz
+
+	//write new file
 	newGenesisStateBz := cdc.MustMarshalJSON(genesisState)
 	exportFile.AppState = newGenesisStateBz
 	exportFile.SaveAs("./exportFile2.json")
