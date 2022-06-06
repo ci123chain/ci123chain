f394708fea689e12d387f42c4851bc26e711f03e
tangshanshan <tangshanshan@corp-ci.com>
2022-06-06 17:50:49 +0800

    add upgrade v1.6.60

--- pkg/upgrade/keeper/keeper.go
+++ pkg/upgrade/keeper/keeper.go
@@ -46,6 +46,9 @@ func NewKeeper(skipUpgradeHeights map[int64]bool, storeKey sdk.StoreKey, cdc *co
 	k.SetUpgradeHandler("UpgradeV1.6.59", func(ctx sdk.Context, info []byte) {
 		k.Logger(ctx).Info("Upgrade successful:", "proposal", "upgrade v1.6.59")
 	})
+	k.SetUpgradeHandler("UpgradeV1.6.60", func(ctx sdk.Context, info []byte) {
+		k.Logger(ctx).Info("Upgrade successful:", "proposal", "upgrade v1.6.60")
+	})
 	return k
 }
 
