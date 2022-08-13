15e517cac804045bf67f242b626a31851a78b8f2
tanhui <tanhui.master@gmail.com>
2022-08-13 17:04:32 +0800

    add recover leveldb

--- pkg/app/cmd/height.go
+++ pkg/app/cmd/height.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/ci123chain/ci123chain/pkg/app"
 	"github.com/spf13/cobra"
+	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/tendermint/tendermint/store"
 	"path/filepath"
 )
@@ -12,10 +13,15 @@ func storeHeight(ctx *app.Context) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "store-height",
 		Short: "Print the store block height",
-		RunE:   func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			rootDir := ctx.Config.RootDir
 			dataDir := filepath.Join(rootDir, "data")
 			originBlockStoreDB, err := openDB(blockStoreDB, dataDir)
+			if err != nil {
+				if _, err = leveldb.RecoverFile(dataDir, nil); err != nil {
+					panic(fmt.Sprintf(`err while recoverfile%s : %s`, dataDir, err.Error()))
+				}
+			}
 			panicError(err)
 			originBlockStore := store.NewBlockStore(originBlockStoreDB)
 			originLatestBlockHeight := originBlockStore.Height()
@@ -25,4 +31,3 @@ func storeHeight(ctx *app.Context) *cobra.Command {
 	}
 	return cmd
 }
-
